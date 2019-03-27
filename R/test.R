ef5041004b14f35ae4286f3e347151145209a0f0
Paul Bettega <Paul.Bettega@univ-grenoble-alpes.fr>
2019-03-27 15:18:17 +0100

    initialisation du package

---
+++ R/test.R
@@ -0,0 +1,14 @@
+#' Test
+#' 
+#' This is a test.
+#'
+#' @return Nothing
+#' @export
+#'
+#' @examples
+#' test()
+test <- function() {
+  print("This is a test.")
+}
+
+# packageNAme:::functionInterne permet d'utiliser une fonction interne
