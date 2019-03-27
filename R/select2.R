01ae3b42db9f851dee46cf8e4da14db70ee31310
Paul Bettega <Paul.Bettega@univ-grenoble-alpes.fr>
2019-03-27 16:02:41 +0100

    Le package passe le check avec la fonction select2

---
+++ R/select2.R
@@ -0,0 +1,23 @@
+
+#' Select variable
+#' 
+#' A poor version of the select() from dplyr.
+#' This fonction allow to select column in a data.frame.
+#' 
+#'
+#' @param df A data frame.
+#' @param ind A vector of character, integer, boolean
+#'
+#' @return A data frame.
+#' @export
+#'
+#' @examples
+#' select2(cars, "speed")
+#' 
+#' select2(cars, 1:2)
+select2 <- function(df, ind){
+  stopifnot(is.character(ind) | is.integer(ind) | is.logical(ind))
+
+  df[ind]
+}
+
