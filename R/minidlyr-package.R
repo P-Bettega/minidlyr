bf304af5cac6c558b1076d8b1096a4a5a306fb15
Paul Bettega <Paul.Bettega@univ-grenoble-alpes.fr>
2019-03-27 17:41:14 +0100

    add a web site with pakgdown

--- R/minidlyr-package.R
+++ R/minidlyr-package.R
@@ -6,3 +6,8 @@
 
 # usethis::use_rcpp()
 # usethis::use_travis()
+# spelling::spell_check_package() test les erreurs de typo dans le package
+# spelling::spell_check_file()
+
+# usethis::use_pkgdown()
+# pkgdown::build_site()
